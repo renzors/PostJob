1a9aa79769d1e33e5dec462843c8f66668218430
Renzo Robles <renzo_rs_8@outlook.com>
2017-04-21 22:05:39 -0500

    Initial commit

---
+++ src/ListenSite/ListenSite/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ListenSite.Models;
+
+namespace ListenSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static List<Site> _sites = new List<Site>();
+        public ActionResult Index()
+        {
+            return View(_sites);
+        }
+
+        [HttpPost]
+        public ActionResult Index(string siteName)
+        {
+            if (_sites.Any(x => x.Name == siteName))
+                _sites.Remove(_sites.FirstOrDefault(x => x.Name == siteName));
+
+            _sites.Add(new Site(){ LastDateTime = DateTime.Now, Name = siteName });
+
+            return View(_sites);
+        }
+    }
+}
---
+++ src/ListenSite/ListenSite/Models/Site.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ListenSite.Models
+{
+    public class Site
+    {
+        public string Name { get; set; }
+        public DateTime LastDateTime { get; set; }
+    }
+}
